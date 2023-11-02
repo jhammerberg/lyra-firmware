ae2e1252937eeb54f0040a09d49bb0fec67a2bad
Jack Hammerberg <jackmh@umich.edu>
2023-11-02 15:52:23 -0400

    Code Clearup, timer, better sd write

--- lyra-firmware/lyra-firmware.ino
+++ lyra-firmware/lyra-firmware.ino
@@ -1,19 +1,19 @@
+/*
+ * lyra-firmware.ino
+ *
+ *  Created on: Nov 2, 2023
+ *      Author: jackmh
+ */
+// ----------------------------------------------------------------------------
 // LIBRARY IMPORTS
 // ----------------------------------------------------------------------------
-// Do not touch unless your custom sensor needs an additional library
 
 #include <SPI.h>
 #include <SD.h>
 
 // ----------------------------------------------------------------------------
-
 // PIN DEFINITIONS
 // ----------------------------------------------------------------------------
-// Note: the pins each thing is connected to may have changed. You will need to
-// reference the schematic for the rocket board here:
-// https://980.engr100.org/project/sensor-board.html
-
-// put pin definitions here
 
 #define PRES_PIN A0
 #define IMU_X A1
@@ -31,167 +31,134 @@
 #define BUZZER 3
 
 // ----------------------------------------------------------------------------
-
-
-
-// CSV FILE HEADER
+// CONSTANTS
 // ----------------------------------------------------------------------------
-// This is the string that goes at the top of your csv file. It is the column 
-// headers for your spreadsheet. You can/should it to match your analog sensors
 
+int startupTime = 30; //seconds
 const int readingInterval = 10; //ms
 const String header = "Time (ms),TMP36 (Raw),Voltage (Raw),Pressure (Raw), Accel X (Raw), Accel Y (Raw), Accel Z (Raw)";
 const String defaultDataFileName = "data";
-String dataPath = defaultDataFileName + ".csv";
-bool firstBoot = true;
+String dataPath = defaultDataFileName + ".csv"; //ok not a constant cause this needs to be changed
 
-// ----------------------------------------------------------------------------
+String takeReadings() {
+  int tmp_raw = analogRead(TMP_PIN);
+  int pres_raw = analogRead(PRES_PIN);
+  int bat_raw = analogRead(BAT_PIN);
+  int x_raw = analogRead(IMU_X);
+  int y_raw = analogRead(IMU_Y);
+  int z_raw = analogRead(IMU_Z);
 
-// HELPER FUNCTIONS
-// ----------------------------------------------------------------------------
-// Functions to call to make setup() and loop() simpler.
-
-// --------------------------Tone function (built-in)--------------------------
-// Play a 1000Hz tone on the buzzer for 2000ms:
-// tone(buzzerPin, 1000, 2000);
-// Play a 440Hz (A4) tone on the buzzer:
-// tone(buzzerPin, 440);
-// This plays until either a new tone is passed
-// or until notone is called:
-// noTone(buzzerPin);
-
-// ----------------------------------------------------------------------------
-
-
-void setup() {
-    //Digital outputs
-    pinMode(BUZZER, OUTPUT);
-    pinMode(LED_1, OUTPUT);
-    pinMode(LED_2, OUTPUT);
-    pinMode(LED_3, OUTPUT);
-    pinMode(LED_4, OUTPUT);
-    pinMode(LED_5, OUTPUT);
-    pinMode(LED_6, OUTPUT);
-
-    Serial.begin(9600);
-    delay(100); // Delay for serial monitor
-
-    Serial.print("Initializing SD card...");
-    if (!SD.begin(SD_CS)) {
-        Serial.println("Card failed, or not present");
-        while (1) { //Warning if the SD Card isn't found
-            tone(BUZZER, 1000, 2000);
-            digitalWrite(LED_2, HIGH);
-            delay(2000);
-            noTone(BUZZER);
-            digitalWrite(LED_2, LOW);
-            delay(2000);
-        }
-    }
-    Serial.println("Card initialized.");
-
-    //Turn on LED to show setup is ok and play a tune
-    digitalWrite(LED_1, HIGH);
-    tone(BUZZER, 1000, 1000);
-    delay(250);
-    tone(BUZZER, 2000, 1000);
-    delay(250);
-    tone(BUZZER, 3000, 1000);
-    delay(250);
-    noTone(BUZZER);
+  String dataString = 
+    String(millis()) + "," + 
+    String(tmp_raw) + "," + 
+    String(bat_raw) + "," + 
+    String(pres_raw) + "," + 
+    String(x_raw) + "," + 
+    String(y_raw) + "," + 
+    String(z_raw);
+  
+  return dataString;
 }
 
-void write(String data) {
-    if((firstBoot == true)) { //Check if the data file already exists and create a new one if it does
-        firstBoot = false;
-        //Serial.println("Data file already exists, creating a new one");
-        int i = 1;
-        while (SD.exists(defaultDataFileName + String(i) + ".csv")) {
-            i++;
-        }
-        dataPath = defaultDataFileName + String(i) + ".csv";
-        File dataFile = SD.open(dataPath, FILE_WRITE);
-        if (!dataFile) { 
-            Serial.println("Error opening file");
-            while (1) { //Warning if the SD Card isn't found
-                tone(BUZZER, 1000, 2000);
-                digitalWrite(LED_2, HIGH);
-                delay(2000);
-                noTone(BUZZER);
-                digitalWrite(LED_2, LOW);
-                delay(2000);
-            }
-        } else {
-            dataFile.println(header);
-            dataFile.close();
-        } //Write the first message to the file
-    }
-    File dataFile = SD.open(dataPath, FILE_WRITE);
-    if (dataFile) {
-        dataFile.println(data);
-        dataFile.close();
-    } else {
-        Serial.println("Error opening file");
-        while (1) { //Warning if the file can't be written.
+void setup() {
+  //Digital outputs
+  pinMode(BUZZER, OUTPUT);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
+  pinMode(LED_5, OUTPUT);
+  pinMode(LED_6, OUTPUT);
+  Serial.begin(9600);
+  delay(100); // Delay for serial monitor
+
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+      Serial.println("Card failed, or not present");
+      while (1) { //Warning if the SD Card isn't found
           tone(BUZZER, 1000, 2000);
           digitalWrite(LED_2, HIGH);
           delay(2000);
           noTone(BUZZER);
           digitalWrite(LED_2, LOW);
           delay(2000);
-        }
+      }
+  }
+  Serial.println("Card initialized.");
+
+  //Creates a new data file if there is already one that exists with the same name
+  int i = 1;
+  while (SD.exists(defaultDataFileName + String(i) + ".csv")) {
+      i++;
+  }
+  dataPath = defaultDataFileName + String(i) + ".csv";
+  File dataFile = SD.open(dataPath, FILE_WRITE);
+  //Write the header & check if the file is open-able
+  if (!dataFile) { 
+      Serial.println("Error opening file");
+      while (1) { //Warning if the SD Card isn't found
+          tone(BUZZER, 1000, 2000);
+          digitalWrite(LED_2, HIGH);
+          delay(2000);
+          noTone(BUZZER);
+          digitalWrite(LED_2, LOW);
+          delay(2000);
+      }
+  } else {
+      dataFile.println(header);
+      dataFile.close();
+  }
+
+  //Turn on LED to show setup is ok and play a tune
+  digitalWrite(LED_1, HIGH);
+  tone(BUZZER, 1000);
+  delay(250);
+  tone(BUZZER, 2000);
+  delay(250);
+  tone(BUZZER, 3000);
+  delay(250);
+  noTone(BUZZER);
+
+  //Countdown timer until sensors start recording
+  delay(2000);
+  tone(BUZZER, 500, 100);
+  int actualTime = (startupTime*1000)+(millis());
+  i = 15;
+  while (millis() < actualTime) { //Will run the loop until the startup time has elapsed
+    tone(BUZZER, 500, 50); //Beep
+    digitalWrite(LED_3, HIGH);
+    delay(startupTime/i); //Delay by a fraction of the startup time
+    digitalWrite(LED_3, LOW);
+    if ((startupTime/(i*2)) < 50) { //Time between beeps gets smaller and smaller until it is below 50ms 
+      i = (i*2);
     }
-}   
-
-String takeReadings() { //Should be about 10ms to sample ADCs
-    int tmp_raw = analogRead(TMP_PIN);
-    int pres_raw = analogRead(PRES_PIN);
-    int bat_raw = analogRead(BAT_PIN);
-    int x_raw = analogRead(IMU_X);
-    int y_raw = analogRead(IMU_Y);
-    int z_raw = analogRead(IMU_Z);
-
-    String dataString = 
-        String(millis()) + "," + 
-        String(tmp_raw) + "," + 
-        String(bat_raw) + "," + 
-        String(pres_raw) + "," + 
-        String(x_raw) + "," + 
-        String(y_raw) + "," + 
-        String(z_raw);
-    
-    return dataString;
+  }
 }
 
 void loop() {
-    if (millis() % readingInterval == 0) { //Take readings every 10ms
-        digitalWrite(LED_1, !digitalRead(LED_1)); //Heartbeat LED
-        String data = takeReadings();
-        write(data);
+  File dataFile = SD.open(dataPath, FILE_WRITE);
+  if (!dataFile) {
+    Serial.println("Error opening file");
+    while (1) { //Warning if the file can't be written.
+      tone(BUZZER, 1000, 2000);
+      digitalWrite(LED_2, HIGH);
+      delay(2000);
+      noTone(BUZZER);
+      digitalWrite(LED_2, LOW);
+      delay(2000);
     }
+  } else {
+    while (1) { //Yes this is a loop inside of a loop but I had to do it so the SD card can be closed.
+      if (millis() % readingInterval == 0) { //Take readings every 10ms
+        String data = takeReadings();
+        dataFile.println(data);
+        dataFile.flush(); //Writes data from buffer to the SD card without having to close and reopen it
+      }
 
-  // flush sd card file outputs
-
-  // do LED and buzzer things
-
-  // For LED: you have 6 different LEDs at your disposal. You must at a minimum
-  // 1. Communicate power status of board (ie on/off)
-  // 2. Communicate when a countdown has been initiated and roughly how long
-  //    until data recording begins
-  // 3. Communicate when data recording is in progress
-
-  // For Buzzer: 
-  // - In general, mirroring LED requirements 2&3 may help you know
-  //  the status of your rocket even if you can't see the LEDs
-  // - You MUST have your buzzer making loud and frequent noises after landing
-  //  following the launch to add in locating your rocket payload
-  // - You will need to utilize some of your sensor data to know when the
-  //  rocket has landed. What sensor(s) you use to determine this is 100% up to
-  //  you. For example, if you had an altitude sensor you could check your
-  //  altitude in every loop and see if it was similar to the altitude when
-  //  launch occurred and turn on the buzzer then. You will need to test this
-  //  thouroughly before launch day as it is how we will find hiding rockets.
-  // - Points may be deducted if your rocket is especially annoying and buzzing
-  //  frequently during the launch preperation and countdown period in front of
-  //  the teaching staff
+      if (millis() % 100 == 0) {
+        digitalWrite(LED_1, !digitalRead(LED_1)); //Heartbeat LED
+      }
+    }
+    dataFile.close();
+  }
 }
